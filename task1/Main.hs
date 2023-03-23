94f99ead484f56c87db8bdb2c2934669854cbdc6
vallghall <romangusev2001@mail.ru>
2023-03-23 23:04:34 +0300

    do the 2nd part of the 1st task in Haskell

--- task1/Main.hs
+++ task1/Main.hs
@@ -3,12 +3,16 @@ module Main where
 import System.Environment (getArgs)
 import System.Directory (doesFileExist)
 import Data.List.Split (splitOn)
+import Data.List (sort)
 
 main :: IO ()
 main = do
     inputFileName <- head <$> getArgs
-    answer <- maximum . processInput <$> readInput inputFileName
-    print answer
+    calories <- processInput <$> readInput inputFileName
+    let max'    = maximum calories
+        top3sum = sum . take 3 . reverse . sort $ calories
+    putStrLn $ "Первая часть: " ++ show max'
+    putStrLn $ "Вторая часть: " ++ show top3sum
 
 readInput :: FilePath -> IO [String]
 readInput fp = do
